97ff8155db62d5bd16968e118b3e3758f1acad53
Adri268 <panteaadriana26@gmail.com>
2023-11-06 21:46:08 +0200

    teme java lab3

---
+++ src/Laboorator3/Cuvinte.java
@@ -0,0 +1,23 @@
+package Laboorator3;
+
+//import java.util.ArrayList;
+//import java.util.Arrays;
+//import java.util.List;
+public class Cuvinte {
+
+    public static void main(String[] args) {
+
+        String a = "university,test,java";
+        String b = "car,university,planes";
+
+        String[] aArray = a.split(",");
+        String[] bArray = b.split(",");
+        for (String aWord : aArray) {
+            for (String bWord : bArray) {
+                if (aWord.equals(bWord)) {
+                    System.out.println("Element comun găsit: " + aWord);
+                }
+            }
+        }
+    }
+}
